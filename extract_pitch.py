07eccdab3b40925864856f48011d47abd3013410
Damith Amarasinghe <hello@damithg.dev>
2023-04-23 12:03:52 +0530

    feat: group to seconds

--- extract_pitch.py
+++ extract_pitch.py
@@ -1,6 +1,28 @@
 import librosa
 import numpy as np
 import json
+from typing import List, Dict
+
+def group_by_second_and_get_median(pitch_data: Dict) -> List[Dict]:
+    grouped = {}
+    for pitch in pitch_data:
+        time = int(pitch['time'])
+        frequency = pitch['frequency']
+        if time in grouped:
+            grouped[time].append(frequency)
+        else:
+            grouped[time] = [frequency]
+
+    result = []
+    for time, frequencies in grouped.items():
+        median_frequency = sum(frequencies) / len(frequencies)
+        result.append({
+            'time': time,
+            'frequency': median_frequency
+        })
+
+    return result
+
 
 audio_path = "audio/Love_Me_Like_You_Do.wav"
 
@@ -25,16 +47,17 @@ for i in range(len(times)):
     frequency_hz = np.nan_to_num(f0[i])
     pitch_data.append({"time": time_sec, "frequency":  frequency_hz })
 
+# Get the median pitch for each second of the recording
+pitch_median = group_by_second_and_get_median(pitch_data)
+
 # Create dictionary with duration and pitch data
-output_dict = {"duration": duration_sec, "pitch": pitch_data}
+output_dict = {"duration": duration_sec, "pitch": pitch_median}
 
 # Convert dictionary to JSON format
 output_json = json.dumps(output_dict)
 
 # Print output to console
 print(output_json)
-
-
 
 
 
